89477576cdb9147b33b97f9d632b4f00aab0331f
CEO-scripts <charlesokorie594@gmail.com>
2024-11-01 19:29:19 +0100

    Using If statements

---
+++ If Statement/app.js
@@ -0,0 +1,10 @@
+//Creating a Sale variable with value of true
+let sale = true
+
+//Reassigning sale value to false
+sale = false
+
+//Putting a conditional Statement
+if(sale){
+  console.log('Time to buy!')
+}
