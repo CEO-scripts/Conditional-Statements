a0eafbfb6008dc51b25751c598c5dd95754509b6
CEO-scripts <charlesokorie594@gmail.com>
2024-11-01 19:47:24 +0100

    Using Comparison Operators

---
+++ Comparison Operators/app.js
@@ -0,0 +1,10 @@
+//Creating variable hungerLevel with value 7
+let hungerLevel = 7
+
+//Creating if..else Statement
+if(hungerLevel > 7){
+  console.log('Time to eat!')
+}
+else{
+  console.log('We can eat later!')
+}
